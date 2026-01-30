0dc618dac4db71f359e56009df263dcf6b5e517c
idrishannn <idrishanndemirdag@gmail.com>
2026-01-30 20:55:21 +0300

    son_hali_1.1

--- src/assets/brand/index.js
+++
@@ -1,6 +0,0 @@
-import { ReactComponent as LogoSvg } from './logo.svg';
-import { ReactComponent as WordmarkSvg } from './wordmark.svg';
-
-export { LogoSvg, WordmarkSvg };
-export const logoPath = '/logo.png';
-export const wordmarkPath = '/wordmark.png';
